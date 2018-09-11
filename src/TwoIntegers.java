4afe78bc1dabee73e8174a189b745a86b05e03e6
bob <bob@APCSA>
2018-09-11 15:41:45 -0700

    Just completed the get and set methods need to finish base code.

---
+++ src/TwoIntegers.java
@@ -0,0 +1,32 @@
+public class TwoIntegers 
+{
+	private int bear;
+	private int dear;
+	
+	public TwoIntegers()
+	{
+		bear = 0;
+		dear = 0;
+	}
+	public TwoIntegers( int bear, int dear)
+	{
+		this.bear = bear;
+		this.dear = dear;
+	}
+	public int getBear()
+	{
+		return bear;
+	}
+	public void setBear( int bear)
+	{
+		this.bear = bear;
+	}
+	public int getDear()
+	{
+		return dear;
+	}
+	public void setDear( int dear)
+	{
+		this.dear = dear;
+	}
+}
